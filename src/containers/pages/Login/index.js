b9bed43f728907cea84423ca48856a28592aefae
Rich <richardofay@gmail.com>
2020-01-24 20:18:05 +0700

    testing deploy

--- src/containers/pages/Login/index.js
+++ src/containers/pages/Login/index.js
@@ -11,7 +11,7 @@ class Login extends Component{
             <div>
                 <p>Login Page {this.props.userName} </p>
                 <button onClick={this.changeUser}>Change username</button>
-                <button>Go to Dashboards Upload</button>
+                <button>Go to Dashboards Uploads</button>
             </div>
         )
     }
