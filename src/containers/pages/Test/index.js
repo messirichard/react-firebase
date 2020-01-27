b0587be3f825a98002f9ccd6b0e43cea7e7f40b2
Rich <richardofay@gmail.com>
2020-01-27 15:56:14 +0700

    Push test

---
+++ src/containers/pages/Test/index.js
@@ -0,0 +1,4 @@
+import React, {Component} from 'react'; import './Test.scss'; import { Button, Navbar, Badge } from 'reactstrap'; class Test extends Component{ render(){ return(
+
+    <div></div>
+    ) } } export default Test;
